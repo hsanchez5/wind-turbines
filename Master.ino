fe44d37323c110820231449f419e5213baf312f7
hsanchez5 <hsanchez5@mail.csuchico.edu>
2016-03-28 00:53:52 -0700

    Added files via upload

---
+++ Master.ino
@@ -0,0 +1,43 @@
+// MASTER
+#include <Wire.h>
+
+
+
+char t[10]={};                        //empty string array to store numbers comming from the slave
+volatile int Val;                     // varaible used by the master to sent data to the slave
+float t_float;                        // takes slave string data and stores into a float var
+
+
+///////////////////////////////////////// SETUP ///////////////////////////////////////////
+void setup() {
+  Wire.begin(8);                       // join i2c bus (address optional for master)
+  Serial.begin(9600);                 // start serial for output
+}
+
+
+
+/////////////////////////////////// LOOP: RECEIVE/SEND DATA /////////////////////////////
+void loop() {
+  Wire.requestFrom(8, 4);             // request 3 bytes from slave device #8
+
+  int i=0;                             //counter for each byte as it arrives
+  while (Wire.available()) {           //gathers data comming from slave
+    t[i] = Wire.read();                // every character that arrives it put in order in the empty array "t"
+    i=i+1;
+  }
+
+    
+  t_float = atof(t);                // reads string sent from slave and stores as a float
+  t_float = map(t_float, 0.00, 1023.00, 1023.00,0.00) - 325.00;     // configure the data correctly
+  t_float = t_float * 5.00/1023.00;           // display voltage value
+
+  Serial.println(t_float);   //shows the data that was converted from a string to voltage float
+  delay(10); //give some time to relax
+
+
+  // send data to slave. here I am just sending the number 2
+  Val=2;
+  Wire.beginTransmission (8);
+  Wire.write (Val);
+  Wire.endTransmission ();
+}
---
+++ Slave.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+
+char t[10];                     //empty array where to put the numbers going to the master
+volatile int Val;               // variable used by the master to sent data to the slave
+
+
+
+////////////////////////////// SETUP ///////////////////////////////////////////////////
+void setup() {
+  Wire.begin(8);                // Slave id #8
+  Wire.onRequest(requestEvent); // fucntion to run when asking for data
+  Wire.onReceive(receiveEvent); // what to do when receiving data
+  Serial.begin(9600);           // serial for displaying data on your screen
+}
+
+
+////////////////////////////////// LOOP: ACTIVE VOLTAGE DIVIDER ///////////////////////////////////////////
+void loop() {
+  int aRead = analogRead(A0);   //plug a potentiometer or a resistor to pin A0, so you can see data being transfer
+
+  // stores the Analog value to a string array called t
+  dtostrf(aRead, 4, 4, t);     //converts the float or integer to a string. (floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, empty array);
+  Serial.println(Val);         // print the character
+  delay(10);
+}
+
+
+/////////////////////////////////INTERRUPTS: RECEIVE/SEND DATA /////////////////////////
+
+// function: what to do when asked for data
+// kind of like interrupts
+void requestEvent() {
+Wire.write(t); 
+}
+
+// what to do when receiving data from master
+void receiveEvent(int howMany)
+{Val = Wire.read();}
